7116b1e75419605869720a41af81b1255dbdb0e2
Abhishek Kaplesh <abkaplesh02@gmail.com>
2024-08-27 22:48:57 +0530

    adding files

---
+++ CyclicSort.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CyclicSort {
+    public static void CSort(int arr[]){
+        int i=0;
+        while(i<arr.length){
+            int correct=arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                int temp=arr[i];
+                arr[i]=arr[correct];
+                arr[correct]=temp;
+            }
+            else{
+                i++;
+            }
+        }
+        System.out.println("The sorted array is :: " + Arrays.toString(arr));
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of array :: ");
+        int a=sc.nextInt();
+        int arr[]=new int[a];
+        System.out.println("Enter the values of array :: ");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("The values of array are :: ");
+        System.out.println(Arrays.toString(arr));
+        CSort(arr);
+    }
+}
