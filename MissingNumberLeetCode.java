638d4f87f084ea682b831644d782923432899864
Abhishek Kaplesh <abkaplesh02@gmail.com>
2024-08-27 22:49:40 +0530

    adding files

---
+++ MissingNumberLeetCode.java
@@ -0,0 +1,46 @@
+//LeetCode 268
+//Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MissingNumberLeetCode {
+    public static void CSort(int arr[]){
+       int i=0;
+       while(i<arr.length){
+           if(arr[i]!=i && arr[i]<arr.length){
+               int temp=arr[arr[i]];
+               arr[arr[i]]=arr[i];
+               arr[i]=temp;
+           }
+           else{
+               i++;
+           }
+       }
+        System.out.println(Arrays.toString(arr));
+
+       for(int j=0;j<arr.length;j++){
+           if(arr[j]!=j){
+               System.out.println("This is missing number");
+               System.out.println(j);
+               return ;
+           }
+       }
+        System.out.println(arr.length);
+
+
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of array :: ");
+        int a=sc.nextInt();
+        int arr[]=new int[a];
+        System.out.println("Enter the values of array :: ");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("The values of array are :: ");
+        System.out.println(Arrays.toString(arr));
+        CSort(arr);
+    }
+
+}
---
+++ SelectionSort.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void SSort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            int minI=i;
+            int minE=arr[i];
+            boolean Check=false;
+            for(int j=i+1;j<arr.length;j++){
+                if (arr[j] < minE) {  // Corrected comparison
+                    minI = j;
+                    minE = arr[j];
+                    Check = true;
+                }
+            }
+            if(Check){
+                int temp=arr[i];
+                arr[i]=minE;
+                arr[minI]=temp;
+            }
+        }
+        System.out.println("The sorted array is :: " + Arrays.toString(arr));
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of array :: ");
+        int a=sc.nextInt();
+        int arr[]=new int[a];
+        System.out.println("Enter the values of array :: ");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("The values of array are :: ");
+        System.out.println(Arrays.toString(arr));
+       SSort(arr);
+    }
+}
